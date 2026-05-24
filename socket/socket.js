9f01b2632ddd785d0fcbeaadabab7e36c26ed4e1
Nazal Malik <mnazal250@gmail.com>
2026-05-24 22:47:35 +0500

    railway url updated

--- socket/socket.js
+++ socket/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("https://caresync-backend-two.vercel.app", {
+const socket = io("https://caresync-backend-production-b0da.up.railway.app", {
   transports: ["websocket"], // ✅ force websocket only
   withCredentials: true
 });
